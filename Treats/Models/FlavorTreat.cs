be9064bc87ec187262b760c334beaaeab6f1d616
Albert Lee <leesga8@gmail.com>
2022-01-15 17:50:54 -0800

    add TreatsContext model, FlavorTreat model

---
+++ Treats/Models/FlavorTreat.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Treats.Models
+{
+  public class TreatsContext : DbContext
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public TreatsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
