80b1c869b099ddc8dc3f4e05ca95ec742a42cc38
Albert Lee <leesga8@gmail.com>
2022-01-15 20:24:39 -0800

    add Delete route and Views for Flavor and Treat

--- Treats/Controllers/FlavorsController.cs
+++ Treats/Controllers/FlavorsController.cs
@@ -88,5 +88,20 @@ namespace Treats.Controllers
     {
       return View();
     }
+
+    public ActionResult Delete (int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
