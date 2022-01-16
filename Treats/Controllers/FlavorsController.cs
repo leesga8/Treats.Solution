f8daaaf0032398aa96263dd182f3a683a7e95100
Albert Lee <leesga8@gmail.com>
2022-01-15 18:13:35 -0800

    add Home index route and views, add Flavors index route and views

---
+++ Treats/Controllers/FlavorsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Treats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Treats
+{
+  public class FlavorsController : Controller
+  {
+    private readonly TreatsContext _db;
+
+    public EngineersController(TreatsContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+          .Include(flavor => flavor.JoinEntities)
+          .ThenInclude(join => join.Treat)
+          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+  }
+}
